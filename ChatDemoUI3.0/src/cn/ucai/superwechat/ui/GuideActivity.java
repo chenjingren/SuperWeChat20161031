f647db314033ad48c38492d7137b6bceaa8adce4
chenjingren <1182508339@qq.com>
2016-11-01 14:57:14 +0800

    添加欢迎页的点击事件

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,7 +1,11 @@
 package cn.ucai.superwechat.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 
 public class GuideActivity extends BaseActivity {
@@ -10,5 +14,18 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_register, R.id.btn_login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_register:
+                startActivity(new Intent(GuideActivity.this,RegisterActivity.class));
+                break;
+            case R.id.btn_login:
+                startActivity(new Intent(GuideActivity.this,LoginActivity.class));
+                break;
+        }
     }
 }
